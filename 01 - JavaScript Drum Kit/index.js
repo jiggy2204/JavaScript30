0632b800b35c187821efe242d80214ab1725614d
Jiggy <jignacio2204@gmail.com>
2022-02-13 19:01:56 -0500

    updated index for js30-01

--- 01 - JavaScript Drum Kit/index.js
+++
@@ -1,8 +0,0 @@
-document.onload = function(){
-    const soundKeys = document.querySelectorAll("div[data-key]");
-    console.log(soundKeys);
-
-    const soundArray = Array.from(soundKeys);
-
-    console.log(soundArray.length);
-}
